1291e4c84da1ccb6285c4ba24bdca76e9cfbd1ae
Jhonatas Galdino <jhonatasgrs@gmail.com>
2025-12-03 21:28:33 -0300

    Implement app.py
    implementando a criação do DB se necessário e também as rotas de listar, adicionar e deletar.

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request, redirect
+import sqlite3
+import os
+
+app = Flask(__name__)
+DB_PATH = "tasks.db"
+
+def init_db():
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS tasks (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              title TEXT NOT NULL
+              )
+    ''')
+    conn.commit()
+    conn.close()
+
+@app.route("/")
+def index():
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor()
+    c.execute("SELECT * FROM tasks")
+    tasks = c.fetchall()
+    conn.close()
+    return render_template("index.html", tasks=tasks)
+
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    if title:
+        conn = sqlite3.connect(DB_PATH)
+        c = conn.cursor()
+        c.execute("INSERT INTO tasks (title) VALUES (?)", (title,))
+        conn.commit()
+        conn.close()
+    return redirect("/")
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor
+    c.execute("DELETE FROM tasks WHERE id = ?", (id,))
+    conn.commit()
+    conn.close()
+    return redirect("/")
+
+if __name__ == "__main__":
+    init_db()
+    app.run(debug=True)
